10ecadab0bd7a0d27f89070f908b13fe40f3d167
Decfox <decfox@localhost.localdomain>
2022-02-01 18:21:07 +0530

    requirements.txt added

---
+++ server/models/speech2text.py
@@ -0,0 +1,61 @@
+import wave
+import json
+from vosk import KaldiRecognizer, Model
+import requests
+import time
+
+API_URL = "https://api-inference.huggingface.co/models/oliverguhr/fullstop-punctuation-multilang-large"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+def punctuate(text):
+   
+    output = query({"inputs": text})
+    time.sleep(5)
+    final = ''
+    for grp in output:
+        final += grp['word']
+        punct = grp["entity_group"]
+        if(punct == '0'):
+            final += ' '
+        else:
+            final += (punct + ' ')
+    
+    with open("input.txt", "w+") as f:
+        f.write(final)
+
+    return final
+
+def SpeechToText(path):
+    wf = wave.open(path, "rb")
+    model = Model("./models/model")
+
+    textResults = []
+
+    recognizer = KaldiRecognizer(model, wf.getframerate())
+
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        if recognizer.AcceptWaveform(data):
+            recognizerResult = recognizer.Result()
+            resultDict = json.loads(recognizerResult)
+            textResults.append(resultDict.get("text", ""))
+
+    resultDict = json.loads(recognizer.FinalResult())
+    textResults.append(resultDict.get("text", ""))
+
+    FinalText = ""
+    for text in textResults:
+        FinalText = FinalText + " " + text
+    
+    print(FinalText)
+    print("-------text generated------")
+
+    result = punctuate(FinalText)
+
+    return result
---
+++ server/models/summary.py
@@ -0,0 +1,95 @@
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+def _create_dictionary_table(text_string) -> dict:
+    #removing stop words
+    stop_words = set(stopwords.words("english"))
+    words = word_tokenize(text_string)
+    #reducing words to their root form
+    stem = PorterStemmer()
+    #creating dictionary for the word frequency table
+    frequency_table = dict()
+    for wd in words:
+        wd = stem.stem(wd)
+        if wd in stop_words:
+            continue
+        if wd in frequency_table:
+            frequency_table[wd] += 1
+        else:
+            frequency_table[wd] = 1
+
+    return frequency_table
+
+def _calculate_sentence_scores(sentences, frequency_table) -> dict:   
+
+    #algorithm for scoring a sentence by its words
+    sentence_weight = dict()
+
+    for sentence in sentences:
+        sentence_wordcount = (len(word_tokenize(sentence)))
+        sentence_wordcount_without_stop_words = 0
+        for word_weight in frequency_table:
+            if word_weight in sentence.lower():
+                sentence_wordcount_without_stop_words += 1
+                if sentence[:4] in sentence_weight:
+                    sentence_weight[sentence[:4]] += frequency_table[word_weight]
+                else:
+                    sentence_weight[sentence[:4]] = frequency_table[word_weight]
+
+        sentence_weight[sentence[:4]] = sentence_weight[sentence[:4]] / sentence_wordcount_without_stop_words
+
+       
+
+    return sentence_weight
+
+def _calculate_average_score(sentence_weight) -> int:
+   
+    #calculating the average score for the sentences
+    sum_values = 0
+    for entry in sentence_weight:
+        sum_values += sentence_weight[entry]
+
+    #getting sentence average value from source text
+    average_score = (sum_values / len(sentence_weight))
+
+    return average_score
+
+def _get_article_summary(sentences, sentence_weight, threshold):
+    sentence_counter = 0
+    article_summary = ''
+
+    for sentence in sentences:
+        if sentence[:4] in sentence_weight and sentence_weight[sentence[:4]] >= (1.25*threshold):
+            article_summary += " " + sentence
+            sentence_counter += 1
+
+    return article_summary
+
+def _run_article_summary(article):
+    
+    #creating a dictionary for the word frequency table
+    frequency_table = _create_dictionary_table(article)
+
+    #tokenizing the sentences
+    sentences = sent_tokenize(article)
+
+    #algorithm for scoring a sentence by its words
+    sentence_scores = _calculate_sentence_scores(sentences, frequency_table)
+
+    #getting the threshold
+    threshold = _calculate_average_score(sentence_scores)
+
+    #producing the summary
+    article_summary = _get_article_summary(sentences, sentence_scores,threshold)
+
+    return article_summary
+
+def generate_summary(path):
+    f = open(path,'r',encoding='utf-8')
+    article_content2=f.read().replace('\n','')
+    f.close()
+    summary_results = _run_article_summary(article_content2)
+    print(summary_results)
+
+    return summary_results
