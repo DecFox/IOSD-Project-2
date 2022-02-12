3c0993b40b838b62d63981d765f038771e624493
Decfox <decfox@localhost.localdomain>
2022-02-12 23:50:38 +0530

    extension integration + server changes

---
+++ client/popup.js
@@ -0,0 +1,25 @@
+function fetchSummary() {
+    fetch("http://localhost:5000/summary")
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+async function startrecording() {
+    params = { active: true, currentWindow: true };
+    tabs = await chrome.tabs.query(params);
+    console.log(tabs);
+    message = {
+        data: "mock data"
+    };
+    var port = await chrome.tabs.connect(tabs[0].id, { name: 'recording' });
+    console.log("connection established");
+    port.postMessage({ message: 'startRecording' });
+}
+
+summary_btn = document.querySelector("#summary");
+document.querySelector("#start").addEventListener("click", startrecording);
+document.querySelector("#summary").addEventListener("click", fetchSummary);
--- client/script.js
+++ client/script.js
@@ -1,104 +1,77 @@
+chrome.runtime.onConnect.addListener( function(port) {
+    console.assert(port.name === 'recording')
+    
+    port.onMessage.addListener((message) => {
+        if(message.message === 'startRecording') {
+            fetch("http://localhost:5000/")
+            .then((res) => {
+                console.log("Successfully initiated server:", res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
 
-    fetch("http://localhost:5000/")
-      .then((res) => {
-        console.log("Successfully initiated server:", res);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+            var socket = io.connect("http://localhost:5000/");
+            socket.on("connect", function (msg) {
+                console.log("connection", socket.connected);
+            });
 
-    vidEl = document.querySelector(".vid");
-    sumEl = document.querySelector(".summary")
+            var mediaRecorder;
+            var chunks = [];
+            var displayMediaOptions = {
+                video: true,
+                audio: true
+            };
 
-    // set chunk frequency time here and duration of video here(in ms)
-    var timeout = 500;
-    var duration = 3000;
+            function BlobToBase64(blob) {
+                return new Promise((resolve, _) => {
+                    const reader = new FileReader();
+                    reader.onloadend = () => resolve(reader.result);
+                    reader.readAsDataURL(blob);
+                });
+            }
 
-    // connecting to server using socketio
-    var socket = io.connect("http://localhost:5000");
-    socket.on("connect", function (msg) {
-      console.log("connection", socket.connected);
-    });
+            function initRecorder(mediaStreamObj) {
+                var mediaRecorder = new MediaRecorder(mediaStreamObj, { mimeType: "video/webm;" });
 
-    // setting diplay option (audio: true makes video unable to build)
-    // set audio: false if only video is required
-    var displayMediaOptions = {
-      video: true,
-      audio: true
-    };
+                mediaRecorder.onstop = () => {
+                    socket.emit("ending", "Chunks finished");
+                    setTimeout(() => { 
+                        socket.disconnect();
+                    }, 5000);
+                };
 
-    var mediaRecorder;
-    var chunks = [];
+                mediaRecorder.ondataavailable = function(ev) {
+                    if(ev.data) {
+                        chunks.push(ev.data);
+                        BlobToBase64(ev.data)
+                            .then(base64str => {
+                            console.log('chunk sent');
+                            socket.emit("videoChunks", base64str);
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                            })
+                    }
+                };
 
-    // setting media stream and starting media recorder
-    navigator.mediaDevices
-      .getDisplayMedia(displayMediaOptions)
-      .then((mediaStreamObj) => {
-        chunks = [];
-        mediaRecorder = initRecorder(mediaStreamObj);
-        console.log("sending chunks....");
-        mediaRecorder.start(500);
-        setTimeout(() => {
-          stoprecord();
-          console.log("finished");
-        }, 180000);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+                return mediaRecorder;
+            }
 
-    // convert blob to base64
-    function blobToBase64(blob) {
-      return new Promise((resolve, _) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.readAsDataURL(blob);
-      });
-    }
+            navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
+                .then((mediaStreamObj) => {
+                    chunks = [];
+                    mediaRecorder = initRecorder(mediaStreamObj);
+                    console.log("sending chunks....");
+                    mediaRecorder.start(500);
+                    mediaStreamObj.oninactive = () => {
+                        mediaRecorder.stop();
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    })
+})
 
-    // initialise media recorder
-    function initRecorder(mediaStreamObj) {
-      var mediaRecorder = new MediaRecorder(mediaStreamObj, {
-        mimeType: "video/webm;",
-      });
-
-      // emit video chunks based on timeout
-      mediaRecorder.ondataavailable = function (ev) {
-        chunks.push(ev.data);
-        // console.log("chunk:", ev.data);
-        blobToBase64(ev.data).then(base64str => {
-            console.log(`b64 client data: ${base64str}`)
-            socket.emit("videoChunks", base64str);
-        })
-      };
-
-      // stopping media recorder and creating blob url for front-end
-      mediaRecorder.onstop = () => {
-        let blob = new Blob(chunks, { type: "video/mp4;" });
-        let videoURL = window.URL.createObjectURL(blob);
-        console.log(chunks);
-        chunks = [];
-        vidEl.src = videoURL;
-      };
-
-      return mediaRecorder;
-    }
-
-    // stopping message emitted to server
-    function stoprecord() {
-      mediaRecorder.stop();
-      socket.emit("ending", "Chunks finished");
-      setTimeout(() => {
-        socket.disconnect()
-      }, 5000)
-    }
-
-    function fetch_summary() {
-      fetch("http://localhost:5000/summary")
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-    }
